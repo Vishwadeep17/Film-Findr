fcae0c59d487cf39595ee6f13d9aebb7302c6b27
rishav6611 <hajariwalarishav@gmail.com>
2023-10-23 15:39:43 +0530

    filmfindr

---
+++ MovieStoreMvc/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using MovieStoreMvc.Models.DTO;
+
+namespace MovieStoreMvc.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+
+        Task<Status> LoginAsync(LoginModel model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(RegistrationModel model);
+    }
+}
